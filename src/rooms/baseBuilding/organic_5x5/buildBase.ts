375bd3019d387b7c141bca989504beb341d9b223
Kali-GG <33780945+Kali-GG@users.noreply.github.com>
2024-11-24 15:55:31 +0100

    finish up placing next constructionSite

--- src/rooms/baseBuilding/organic_5x5/buildBase.ts
+++ src/rooms/baseBuilding/organic_5x5/buildBase.ts
@@ -18,37 +18,40 @@ const STRUCTURE_BUILD_PRIORITY = [
 const buildBase =  (room: Room): void => {
   //todo: find a smart way to make sure this expensive operation only runs when needed
 
-  if (Object.keys(Game.constructionSites).length >= MAX_CONSTRUCTION_SITES) { return; }
-  if (!room.memory.baseLayout) { return; }
   if (!room._my() || !room.controller) { return; }
+  if (!room.memory.baseLayout) { return; }
+  if (Object.keys(Game.constructionSites).length >= MAX_CONSTRUCTION_SITES) { return; }
   let controller = room.controller;
+  let baseLayout = room.memory.baseLayout;
   if (room.find(FIND_MY_CONSTRUCTION_SITES).length > 0) { return; }
 
   STRUCTURE_BUILD_PRIORITY.every( type => {
     if (type == STRUCTURE_RAMPART || type == STRUCTURE_ROAD || type == STRUCTURE_WALL) { return true; }
 
     let filteredStructuresArr = findMyStructures(type, room);
-    if (filteredStructuresArr.length < CONTROLLER_STRUCTURES[type][controller.level]) {
-      room.memory.baseLayout?.structurePositions[type].every( structurePos => {
-        if (_.filter(filteredStructuresArr, (current) => {
-          return current.pos.x == structurePos.x && current.pos.y == structurePos.y;
-        }).length == 0) {
-          // place a building!
+    if (!structureNeedsToBeBuilt(filteredStructuresArr, type, controller)) { return true; }
 
-        }
-      });
-      // find spot
-      // place structure &
-      // return false; // breaks out of .every
-    }
-    return true;
+    placeNextBuilding(room, filteredStructuresArr, baseLayout, type);
+
+    return false;
   });
 
 }
 
+const structureNeedsToBeBuilt = (filteredStructuresArr: Structure[], type: BuildableStructureConstant, controller: StructureController) => {
+  return filteredStructuresArr.length < CONTROLLER_STRUCTURES[type][controller.level];
+}
 
-const placeNextBuilding = (room: Room) => {
+const placeNextBuilding = (room: Room, filteredStructuresArr: Structure[], baseLayout: BaseLayoutMemory, type: BuildableStructureConstant) => {
+  baseLayout.structurePositions[type].every( structurePos => {
+    if (_.filter(filteredStructuresArr, (current) => {
+      return current.pos.x == structurePos.x && current.pos.y == structurePos.y;
+    }).length > 0) { return true; }
 
+    room.createConstructionSite(structurePos.x, structurePos.y, type);
+    //todo: check return values, normally it should be fine, but -7 ERR_INVALID_TARGET might mean that there is a structure already in this location
+    return false;
+  });
 }
 
 const findMyStructures = (type: BuildableStructureConstant, room?: Room) => {
@@ -58,4 +61,4 @@ const findMyStructures = (type: BuildableStructureConstant, room?: Room) => {
 }
 
 
-export  {};
+export  {buildBase};
